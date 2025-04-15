29032aec0ccbad9bfa841c8aa0874ab157859c18
j-void <janmeshukey@gmail.com>
2025-04-15 17:39:18 +0530

    cosmos test-eval

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -62,7 +62,50 @@ class CosmosDataset(Dataset):
 
         return image.float(), bboxes, caption_match, caption_diff
 
+class CosmosTestDataset(Dataset):
+    def __init__(self, json_file, img_dir, transform_full=None):
+        """
+        Args:
+            json_file (str): Path to the JSON file with samples.
+            img_dir (str): Directory containing images.
+            transform_full (callable, optional): Transformation for the full image.
+        """
+        self.data = []
+        with open(json_file, 'r') as f:
+            for line in f:
+                self.data.append(json.loads(line))
 
+        self.img_dir = img_dir
+        self.transform_full = transform_full
+        self.resize = Resize(512)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        example = self.data[idx]
+        img_path = os.path.join(self.img_dir, example["img_local_path"])
+        try:
+            image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+        except Exception as e:
+            raise ValueError(f"Error loading image at {img_path}: {e}")
+        bboxes = np.array(example.get("maskrcnn_bboxes", []))
+        image, bboxes = self.resize(image, bboxes)
+        _, H, W = image.shape
+        bboxes = list(bboxes)[:10]
+        bboxes.append([0, 0, W, H])
+        bboxes = [torch.tensor(bbox) for bbox in bboxes]
+
+        if self.transform_full:
+            image = self.transform_full(image)        
+
+        caption1 = example['caption1_modified']
+        caption2 = example['caption2_modified']
+        
+        label = torch.tensor(example['context_label']).long()
+        bert_score = torch.tensor(example['bert_base_score']).float()
+
+        return image.float(), bboxes, caption1, caption2, label, bert_score
 
 
 if __name__ == "__main__":
--- utils/misc.py
+++ utils/misc.py
@@ -28,3 +28,64 @@ def save_model(epoch, model, optimizer, scheduler, path):
         'optimizer_state_dict': optimizer.state_dict(),
         'scheduler_state_dict': scheduler.state_dict() if scheduler else None,
     }, path)
+    
+def top_bbox_from_scores(bboxes, scores):
+    """
+        Returns the top matching bounding box based on scores
+
+        Args:
+            bboxes (list): List of bounding boxes for each object
+            scores (list): List of scores corresponding to bounding boxes given by bboxes
+
+        Returns:
+            matched_bbox: The bounding box with the maximum score
+    """
+    bbox_scores = [(bbox, score) for bbox, score in zip(bboxes, scores)]
+    sorted_bbox_scores = sorted(bbox_scores, key=lambda x: x[1], reverse=True)
+    matched_bbox = sorted_bbox_scores[0][0]
+    return matched_bbox
+
+def bb_intersection_over_union(boxA, boxB):
+    """
+        Computes IoU (Intersection over Union for 2 given bounding boxes)
+
+        Args:
+            boxA (list): A list of 4 elements holding bounding box coordinates (x1, y1, x2, y2)
+            boxB (list): A list of 4 elements holding bounding box coordinates (x1, y1, x2, y2)
+
+        Returns:
+            iou (float): Overlap between 2 bounding boxes in terms of overlap
+    """
+
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection area and dividing it by the sum of both boxes
+    # intersection area / areas - the intersection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
+
+def is_bbox_overlap(bbox1, bbox2, iou_overlap_threshold):
+    """
+        Checks if the two bounding boxes overlap based on certain threshold
+
+        Args:
+            bbox1: The coordinates of first bounding box
+            bbox2: The coordinates of second bounding box
+            iou_overlap_threshold: Threshold value beyond which objects are considered overlapping
+
+        Returns:
+            Boolean whether two boxes overlap or not
+    """
+    iou = bb_intersection_over_union(boxA=bbox1, boxB=bbox2)
+    if iou >= iou_overlap_threshold:
+        return True
+    return False
